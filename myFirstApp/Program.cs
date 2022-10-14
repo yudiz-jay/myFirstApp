05270be72698a839f11ccc502c09e15fb7d362a0
Jay Pipaliya <113347625+yudiz-jay@users.noreply.github.com>
2022-10-14 14:04:01 +0530

    Add files via upload

---
+++ myFirstApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace myFirstApp
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int reminder,temp;
+            int sum = 0;
+            temp = number;
+            while(number > 0)
+            {   
+                reminder = number % 10;
+                number = number / 10;
+                sum  = sum * 10 + reminder;
+            }
+        
+            if(temp == sum)
+            {
+                Console.WriteLine("The number is palindrome number");
+            }
+
+            else
+            {
+                Console.WriteLine("The number is not palindrome number");     
+            }
+        }
+
+    }
+}
